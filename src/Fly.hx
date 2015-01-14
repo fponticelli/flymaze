20993e348d8ab526a919a32de4f60ee003f8c427
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-01-13 23:46:52 -0700

    improved defaults and added trail

--- src/Fly.hx
+++ src/Fly.hx
@@ -1,23 +1,31 @@
 class Fly {
-  public var x : Float;
-  public var y : Float;
-  public var v : Float;
-  public var d : Float;
-  public var maxSteering = 30.0;
+  public var x(default, null) : Float;
+  public var y(default, null) : Float;
+  public var v(default, null) : Float;
+  public var d(default, null) : Float;
+  public var maxSteering = 15.0;
+  public var trail(default, null) : Array<{ x : Float, y : Float }>;
+  public var trailPos(default, null) : Int;
 
   var _steer : Int;
 
-  public function new() {
-    x = 0.0;
-    y = 0.0;
-    v = 1.0;
+  public function new(x, y) {
+    this.x = x;
+    this.y = y;
+    v = 3;
     d = -Math.PI / 2;
+    trail = [for(i in 0...25) { x : x, y : y }];
+    trailPos = 0;
   }
 
   public function update() {
     d += maxSteering * _steer / 180 * Math.PI;
     var dx = Math.cos(d) * v,
         dy = Math.sin(d) * v;
+    trail[trailPos].x = x;
+    trail[trailPos].y = y;
+    if(++trailPos == trail.length)
+      trailPos = 0;
     x += dx;
     y += dy;
     _steer = 0;
