382fd6abcbadd95cb44b749861c92adf4f8320f2
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-01-17 16:33:14 -0700

    implemented UpdatePosition

--- src/fly/systems/UpdatePosition.hx
+++ src/fly/systems/UpdatePosition.hx
@@ -8,10 +8,14 @@ import fly.components.Velocity;
 
 using thx.core.Tuple;
 
-class UpdatePosition implements ISystem<Tuple3<Position, Direction, Velocity>> {
+class UpdatePosition implements ISystem {
   public function new() {}
 
-  public function process(t : Tuple3<Position, Direction, Velocity>) {
-
+  public function update(position : Position, direction : Direction, velocity : Velocity) {
+    position.x += direction.dx * velocity.value;
+    position.y += direction.dy * velocity.value;
   }
+
+  public function getRequirements() : Array<Class<Dynamic>>
+    return [Position, Direction, Velocity];
 }
