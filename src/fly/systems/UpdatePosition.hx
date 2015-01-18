f927f8af5c7c341a5a83bf1a2eed44b5cf590f86
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-01-17 17:30:48 -0700

    cleanup

--- src/fly/systems/UpdatePosition.hx
+++ src/fly/systems/UpdatePosition.hx
@@ -6,8 +6,6 @@ import fly.components.Direction;
 import fly.components.Position;
 import fly.components.Velocity;
 
-using thx.core.Tuple;
-
 class UpdatePosition implements ISystem {
   public function new() {}
 
