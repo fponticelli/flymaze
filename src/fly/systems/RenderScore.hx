ebff29fb5746b651268cc3664c2980779f361476
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-01-19 15:22:53 -0700

    changed font for score

--- src/fly/systems/RenderScore.hx
+++ src/fly/systems/RenderScore.hx
@@ -11,7 +11,7 @@ class RenderScore implements ISystem {
     this.mini = mini;
 
   public function update(score : Score) {
-    mini.ctx.font = "12px menlo";
+    mini.ctx.font = "16px 'Montserrat', sans-serif";
     mini.ctx.fillStyle = "#000000";
     mini.ctx.fillText('${score.value}', 10, 20);
   }
