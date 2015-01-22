9b812ae613cd52b47973d7b8b3fc413ae0368aa2
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-01-21 23:49:38 -0700

    use IComponent in Position

--- src/fly/components/Position.hx
+++ src/fly/components/Position.hx
@@ -1,14 +1,6 @@
 package fly.components;
 
-class Position {
-  public function new(x : Float, y : Float) {
-    this.x = x;
-    this.y = y;
-  }
-
-  public var x : Float;
-  public var y : Float;
-
-  public function toString()
-    return 'Position($x, $y)';
+class Position implements edge.IComponent {
+  var x : Float;
+  var y : Float;
 }
