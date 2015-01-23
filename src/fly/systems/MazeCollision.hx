ac2e18ef315b5e2eac00c03b1c4b2d48ab3b655b
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-01-22 23:41:32 -0700

    fixed collision corner case (can be simplified)

--- src/fly/systems/MazeCollision.hx
+++ src/fly/systems/MazeCollision.hx
@@ -3,6 +3,7 @@ package fly.systems;
 import amaze.Maze;
 import edge.*;
 import fly.components.*;
+using thx.core.Floats;
 
 class MazeCollision implements ISystem {
   static inline var E = 0.00001;
@@ -28,6 +29,97 @@ class MazeCollision implements ISystem {
     } else if(drow == row) {
       if(dcol < col && !cell.left || dcol > col && !cell.right)
         d.angle = -d.angle + Math.PI;
+    } else if(dcol < col && drow < row) {
+      if(pos(col * cellSize, row * cellSize, p.x, p.y, dx, dy) > 0) {
+        if(!cell.top) {
+          if(!cell.left) {
+            d.angle += Math.PI;
+          } else {
+            d.angle = -d.angle;
+          }
+        } else if(null != maze.cells[row-1][col] && !maze.cells[row-1][col].left) {
+          d.angle = -d.angle + Math.PI;
+        }
+      } else {
+        if(!cell.left) {
+          if(!cell.top) {
+            d.angle += Math.PI;
+          } else {
+            d.angle = -d.angle + Math.PI;
+          }
+        } else if(null != maze.cells[row][col-1] && !maze.cells[row][col-1].top) {
+          d.angle = -d.angle;
+        }
+      }
+    } else if(dcol > col && drow > row) {
+      if(pos(col * cellSize, row * cellSize, p.x, p.y, dx, dy) > 0) {
+        if(!cell.bottom) {
+          if(!cell.right) {
+            d.angle += Math.PI;
+          } else {
+            d.angle = -d.angle;
+          }
+        } else if(null != maze.cells[row+1][col] && !maze.cells[row+1][col].right) {
+          d.angle = -d.angle + Math.PI;
+        }
+      } else {
+        if(!cell.right) {
+          if(!cell.bottom) {
+            d.angle += Math.PI;
+          } else {
+            d.angle = -d.angle + Math.PI;
+          }
+        } else if(null != maze.cells[row][col+1] && !maze.cells[row][col+1].bottom) {
+          d.angle = -d.angle;
+        }
+      }
+    } else if(dcol < col && drow > row) {
+      if(pos(col * cellSize, row * cellSize, p.x, p.y, dx, dy) > 0) {
+        if(!cell.bottom) {
+          if(!cell.left) {
+            d.angle += Math.PI;
+          } else {
+            d.angle = -d.angle;
+          }
+        } else if(null != maze.cells[row+1][col] && !maze.cells[row+1][col].left) {
+          d.angle = -d.angle + Math.PI;
+        }
+      } else {
+        if(!cell.left) {
+          if(!cell.bottom) {
+            d.angle += Math.PI;
+          } else {
+            d.angle = -d.angle + Math.PI;
+          }
+        } else if(null != maze.cells[row][col-1] && !maze.cells[row][col-1].bottom) {
+          d.angle = -d.angle;
+        }
+      }
+    } else if(dcol > col && drow < row) {
+      if(pos(col * cellSize, row * cellSize, p.x, p.y, dx, dy) > 0) {
+        if(!cell.top) {
+          if(!cell.right) {
+            d.angle += Math.PI;
+          } else {
+            d.angle = -d.angle;
+          }
+        } else if(null != maze.cells[row-1][col] && !maze.cells[row-1][col].right) {
+          d.angle = -d.angle + Math.PI;
+        }
+      } else {
+        if(!cell.right) {
+          if(!cell.top) {
+            d.angle += Math.PI;
+          } else {
+            d.angle = -d.angle + Math.PI;
+          }
+        } else if(null != maze.cells[row][col+1] && !maze.cells[row][col+1].top) {
+          d.angle = -d.angle;
+        }
+      }
     }
   }
+
+  function pos(x : Float, y : Float, ax : Float, ay : Float, bx : Float, by : Float)
+    return ((bx - ax) * (y - ay) - (by - ay) * (x - ax)).sign();
 }
