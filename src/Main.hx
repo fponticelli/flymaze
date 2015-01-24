83be2d05b8f473a83c9fd08fc6d705a296eefd18
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-01-24 16:38:01 -0700

    removed sui dependency

--- src/Main.hx
+++ src/Main.hx
@@ -1,5 +1,4 @@
 import fly.Config;
-import sui.Sui;
 
 import fly.Game;
 import fly.components.Position;
@@ -13,14 +12,6 @@ class Main {
         game = new fly.Game(mini, config);
     game.start();
     decorateBackground();
-/*
-    var sui = new Sui();
-    sui.bind(maze.width);
-    sui.bind(maze.height);
-    sui.bind(cellSize);
-    sui.attach();
-    //sui.bind(function() maze.generate.bind(rows - 1, Math.floor(cols / 2)));
-*/
   }
 
   public static function decorateBackground() {
