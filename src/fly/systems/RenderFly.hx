2e2ca1681c757d417b2a3dc2bdd7a7b91ddbd9f3
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-01-24 15:06:24 -0700

    better Fly rendering

--- src/fly/systems/RenderFly.hx
+++ src/fly/systems/RenderFly.hx
@@ -13,8 +13,8 @@ class RenderFly implements ISystem {
   public function update(position : Position, f : Fly) {
     var p = Math.random() * 6 - 3;
     mini.dot(position.x + f.height, position.y + f.height * 2, 2.5, 0x00000044);
-    mini.dot(position.x - 4.5 - p / 3, position.y + p, 2, 0xCCFFEEEE);
-    mini.dot(position.x + 4.5 + p / 3, position.y + p, 2, 0xCCFFEEEE);
+    mini.dot(position.x - 4.5 - p / 3, position.y + p, 2, 0xEEFFEEFF);
+    mini.dot(position.x + 4.5 + p / 3, position.y + p, 2, 0xEEFFEEFF);
     mini.dot(position.x, position.y, 1.5, 0x000000FF);
   }
 }
