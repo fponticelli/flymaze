adb3ac805732856876f426a722cb0c6a9ca8f535
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-01-24 11:55:28 -0700

    highlight score

--- src/fly/systems/RenderScore.hx
+++ src/fly/systems/RenderScore.hx
@@ -12,6 +12,9 @@ class RenderScore implements ISystem {
 
   public function update(score : Score) {
     mini.ctx.font = "16px 'Montserrat', sans-serif";
+    mini.ctx.lineWidth = 4;
+    mini.ctx.strokeStyle = "#FFFFFF";
+    mini.ctx.strokeText('${score.value}', 10, 20);
     mini.ctx.fillStyle = "#000000";
     mini.ctx.fillText('${score.value}', 10, 20);
   }
