b30457470d025b1e321a269d4af1c0b48d8db693
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-01-24 15:06:47 -0700

    droplet life

--- src/fly/components/Droplet.hx
+++ src/fly/components/Droplet.hx
@@ -7,9 +7,12 @@ import thx.color.*;
 class Droplet implements edge.IComponent {
   var radius : Float;
   var color : RGB;
+  var life : Int;
+  public static var maxLife = 100;
   public static function create()
     return new Droplet(
       Math.random() * 0.5 + 1.2,
-      HSL.create(20 + 30 * Math.random(), Math.random() * 0.4 + 0.6, 0.3)
+      HSL.create(20 + 30 * Math.random(), Math.random() * 0.4 + 0.6, 0.3),
+      maxLife
     );
 }
