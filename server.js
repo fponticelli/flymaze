0a16580ecff25719620abde3de213f913ca1fd56
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-01-31 14:02:38 -0500

    updated server to persist data

--- server.js
+++ server.js
@@ -1,27 +1,40 @@
-var app    = require('express')(),
-    server = require('http').Server(app),
-    io     = require('socket.io')(server),
-    static = require('serve-static');
-
-server.listen(process.env.PORT || 5000);
-
-app.use(static('bin/', {}));
+var app     = require('express')(),
+    server  = require('http').Server(app),
+    io      = require('socket.io')(server),
+    static  = require('serve-static'),
+    mongodb = require('mongodb'),
+    MongoClient = mongodb.MongoClient;
 
 var scoreTable = [],
-    players = {},
     topScores = 20,
-    gameSessions = {};
+    scores,
+    players,
+    playersCache = {};
 
 function getPlayerName(id) {
-  return players[id];
+  return playersCache[id] && playersCache[id].name;
 }
 
 function setPlayerName(id, name) {
-  players[id] = name;
+  if(playerExists(id)) {
+    playersCache[id].name = name;
+  } else {
+    playersCache[id] = { id : id, name : name };
+  }
+  players
+    .update(
+      { id : id },
+      { id : id, name : name },
+      { upsert : true },
+      function(err, result) {
+        if(err)
+          console.error(err);
+      }
+    );
 }
 
 function playerExists(id) {
-  return !!players[id];
+  return !!playersCache[id];
 }
 
 function getTopLen() {
@@ -56,7 +69,13 @@ function insertScore(data) {
   sortScoreTable();
 }
 
-function updateScore(/*data*/) {
+function updateScore(data, final) {
+  if(final) {
+    scores.insert(data, function(err, result) {
+      if(err)
+        console.error(err);
+    });
+  }
   sortScoreTable();
 }
 
@@ -74,8 +93,43 @@ function getTopPlayers() {
   return list;
 }
 
+MongoClient.connect(process.env.MONGOSOUP_URL || "mongodb://localhost", function(err, db) {
+  if(err) {
+    console.log("failed to connect to the database");
+  } else {
+    console.log("connected to database");
+  }
+  server.listen(process.env.PORT || 5000);
+  app.use(static('bin/', {}));
+
+  players = db.collection('players');
+  players.find({})
+    .toArray(function(err, docs) {
+      if (err) {
+        return console.error(err)
+      }
+      docs.forEach(function(doc) {
+        players[doc.id] = doc;;
+      });
+    });
+
+  scores = db.collection('scores');
+  scores.find({})
+    .sort({ score : -1 })
+    .limit(topScores)
+    .toArray(function(err, docs) {
+      if (err) {
+        return console.error(err)
+      }
+      docs.forEach(function(doc) {
+        scoreTable.push(doc);
+      });
+    });
+});
+
 io.on('connection', function (socket) {
   socket.emit('request:id');
+  var latestScore;
 
   // id, name
   socket.on('id:confirm', function(data) {
@@ -98,14 +152,13 @@ io.on('connection', function (socket) {
 
   // id, score, level, time, gameid
   socket.on('score:play', function (data) {
-    if(!gameSessions[data.gameid]) {
-      gameSessions[data.gameid] = data;
+    if(!latestScore) {
       insertScore(data);
+      latestScore = data;
     } else {
-      var o = gameSessions[data.gameid];
-      o.score = data.score;
-      o.level = data.level;
-      o.time = data.time;
+      latestScore.score = data.score;
+      latestScore.level = data.level;
+      latestScore.time = data.time;
       updateScore(data);
     }
     if(isTopScore(data.score)) {
@@ -117,13 +170,19 @@ io.on('connection', function (socket) {
 
   // id, score, level, time, gameid
   socket.on('score:end', function (data) {
-    if(!gameSessions[data.gameid]) return;
-    updateScore(data);
-    delete gameSessions[data.gameid];
+    updateScore(data, true);
     if(isTopScore(data.score)) {
       var players = getTopPlayers();
       socket.broadcast.emit('leaderboard:top', players);
       socket.emit('leaderboard:top', players);
+    }
+    latestScore = null;
+  });
+
+  socket.on('disconnect', function() {
+    if(latestScore) {
+      updateScore(latestScore, true);
+      latestScore = null;
     }
   });
 });
